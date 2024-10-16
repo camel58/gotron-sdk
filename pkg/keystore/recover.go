55f6fa3d1dfca1a9d8828115c4fae8df797423b9
camel58 <zhang58san@gmail.com>
2024-10-16 18:52:52 +0800

    init block

--- pkg/keystore/recover.go
+++ pkg/keystore/recover.go
@@ -5,16 +5,16 @@ package keystore
 
 import (
 	"github.com/camel58/gotron-sdk/pkg/address"
-	"github.com/ethereum/go-ethereum/crypto/secp256k1"
 )
 
 func RecoverPubkey(hash []byte, signature []byte) (address.Address, error) {
+	return nil, nil
 
 	if signature[64] >= 27 {
 		signature[64] -= 27
 	}
 
-	sigPublicKey, err := secp256k1.RecoverPubkey(hash, signature)
+	sigPublicKey, err := RecoverPubkey(hash, signature)
 	if err != nil {
 		return nil, err
 	}
